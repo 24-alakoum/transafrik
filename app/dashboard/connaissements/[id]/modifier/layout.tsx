a0de386b13653d56c46d6c070dd4493eb1060546
alakoum <mahamadoualakoum@gmail.com>
2026-08-14 10:54:43 +0000

    enleve

--- app/dashboard/connaissements/[id]/modifier/layout.tsx
+++ app/dashboard/connaissements/[id]/modifier/layout.tsx
@@ -1,1 +1,10 @@
-// Layout removed to revert BL modifications
+import React, { ReactNode } from 'react';
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <section className="p-4 bg-gray-50 min-h-screen">
+      {children}
+    </section>
+  );
+}
+
